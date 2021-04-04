a0f38b3a79d58ea584a2c2d7ab0a477dfddd7051
Afifah Khairunnisa <72483977+newrnfi@users.noreply.github.com>
2021-04-04 09:07:41 +0700

    Add files via upload

---
+++ prak1/form_nilai.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Nilai</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <style>
+        .form-control {
+            width: 400px;
+        }
+        .form-select {
+            width: 400px;
+        }
+        .isi {
+            margin-left: 380px;
+            margin-top: 25px;
+            width: 800px;
+        }
+        
+    </style>
+</head>
+<body>
+    <div class="bg-dark p-4">
+    <h2 class="mt-2 text-light bg-dark">Sistem Penilaian</h2>
+    </div>
+    <div class="mx-4">
+    <hr class="mb-0 mt-0">
+    
+    <form class="form-horizontal" method="GET" action="array_siswa.php">
+    <fieldset>
+
+    <!-- Form Name -->
+    <legend>Form Nilai Siswa</legend>
+    <hr class="mb-2 mt-0">
+    <div class="isi">
+        <div class="form-group">
+        <div class="form-group row mb-2">
+            <label for="" class="col-sm-3 col-form-label">Nama Lengkap </label>
+            <div class="col-sm-9">
+            <input type="text" name="nama" class="form-control" id="">
+        </div>
+        </div>
+        <div class="form-group">
+        <div class="form-group row mb-2">
+            <label for="" class="col-sm-3 col-form-label">NIM </label>
+            <div class="col-sm-9">
+            <input type="text" name="nim" class="form-control" id="">
+        </div>
+        </div>
+        <div class="form-group row mb-2">
+            <label for="" class="col-sm-3 col-form-label">Mata Kuliah </label>
+            <div class="col-sm-9">
+            <select class="form-select mb-2" name="matkul" aria-label="Default select example">
+            <option selected>Pilih</option>
+            <option value="UI/UX">UI/UX</option>
+            <option value="Basis Data">Basis Data</option>
+            <option value="Pemrograman Web">Pemrograman Web</option>
+            <option value="Bahasa Inggris">Bahasa Inggris</option>
+            <option value="Statistik">Statistik</option>
+            <option value="KK">KK</option>
+            <option value="PPKn">PPKn</option>
+            <option value="Jaringan Komputer">Jaringan Komputer</option>
+            </select>
+        </div>
+        <div class="form-group">
+        <div class="form-group row mb-2">
+            <label for="" class="col-sm-3 col-form-label">Nilai UTS</label>
+            <div class="col-sm-9">
+            <input type="text" name="nilai_uts" class="form-control" id="">
+        </div>
+        </div>
+        <div class="form-group">
+        <div class="form-group row mb-2">
+            <label for="" class="col-sm-3 col-form-label">Nilai UAS</label>
+            <div class="col-sm-9">
+            <input type="text" name="nilai_uas" class="form-control" id="">
+        </div>
+        </div>
+        <div class="form-group">
+        <div class="form-group row mb-2">
+            <label for="" class="col-sm-3 col-form-label">Nilai Tugas/Praktikum</label>
+            <div class="col-sm-9">
+            <input type="text" name="nilai_tugas" class="form-control" id="">
+        </div>
+        </div>
+        <div class="form-group row mb-2">
+            <label for="" class="col-sm-3 col-form-label"></label>
+            <div class="col-sm-9">
+            <input type="submit" value="Simpan" name="proses" class="btn btn-primary"/>
+        </div>
+    
+        </div>
+    </fieldset>
+    </form>
+    <br/>
+    <!-- <?php
+    $proses= $_GET['proses'];
+    $nama_siswa = $_GET['nama'];
+    $mata_kuliah = $_GET['matkul'];
+    $nilai_uts = $_GET['nilai_uts'];
+    $nilai_uas = $_GET['nilai_uas'];
+    $nilai_tugas = $_GET['nilai_tugas'];
+    echo 'Proses : '.$proses;
+    echo '<br/>Nama : '.$nama_siswa;
+    echo '<br/>Mata Kuliah : '.$mata_kuliah;
+    echo '<br/>Nilai UTS : '.$nilai_uts;
+    echo '<br/>Nilai UAS : '.$nilai_uas;
+    echo '<br/>Nilai Tugas Praktikum : '.$nilai_tugas;
+    ?> -->
+
+    </div>
+</body>
+</html>
